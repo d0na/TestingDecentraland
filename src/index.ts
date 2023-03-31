595f51b4c5612d94706a667a19e4499b6a5e90a3
Francesco <francesco.donini@iit.cnr.it>
2023-03-31 14:55:21 +0200

    chore: added new model (Avocad)  as a test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { engine, executeTask, Material } from '@dcl/sdk/ecs'
-import { Color4 } from '@dcl/sdk/math'
+import { engine, executeTask, GltfContainer, Material, Transform } from '@dcl/sdk/ecs'
+import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
 
 import { createCube } from './factory'
 import { bounceScalingSystem, circularSystem, spawnerSystem } from './systems'
@@ -18,7 +18,19 @@ engine.addSystem(bounceScalingSystem)
 executeTask(async function () {
   // Create my main cube and color it.
   const cube = createCube(8, 1, 8)
-  Material.setPbrMaterial(cube, { albedoColor: Color4.create(1.0, 0.85, 0.42) })
+  Material.setPbrMaterial(cube, { albedoColor: Color4.create(2.0, 0.85, 0.42) })
 })
 
 setupUi()
+
+
+// Testing new model Avocado
+let avocado = engine.addEntity()
+
+GltfContainer.create(avocado, {src: ("models/avocado.gltf")})
+
+Transform.create(avocado, {
+	  position:  Vector3.create(3, 1, 3),
+	  scale: Vector3.create(10, 10, 10)
+	})
+
